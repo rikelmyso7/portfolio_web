c391d406b5f6aa812860c762dd1137c3a9691a16
rikelmyso7 <rikelmyroberto1@gmail.com>
2023-11-27 18:42:05 -0300

    tt
    Cars addiction

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -99,6 +99,60 @@ function home() {
   window.location.replace("https://rikelmyso7.github.io/portfolio/index.html");
 }
 
-function models() {
-  window.location.replace("https://rikelmyso7.github.io/portfolio/pages/models.html");
+function pokemon() {
+  window.location.replace("https://rikelmyso7.github.io/portfolio/pages/pokemon_models.html");
 }
+
+function clothes() {
+  window.location.replace("https://rikelmyso7.github.io/portfolio/pages/clothes_models.html");
+}
+
+function props() {
+  window.location.replace("https://rikelmyso7.github.io/portfolio/pages/props_models.html");
+}
+
+// Cards selection
+const cardsContainer = document.querySelector(".cards");
+const cardsContainerInner = document.querySelector(".cards__inner");
+const cards = Array.from(document.querySelectorAll(".card"));
+const overlay = document.querySelector(".overlay");
+
+const applyOverlayMask = (e) => {
+  const overlayEl = e.currentTarget;
+  const x = e.pageX - cardsContainer.offsetLeft;
+  const y = e.pageY - cardsContainer.offsetTop;
+
+  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+};
+
+const createOverlayCta = (overlayCard, ctaEl) => {
+  const overlayCta = document.createElement("div");
+  overlayCta.classList.add("cta");
+  overlayCta.textContent = ctaEl.textContent;
+  overlayCta.setAttribute("aria-hidden", true);
+  overlayCard.append(overlayCta);
+};
+
+const observer = new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+    const cardIndex = cards.indexOf(entry.target);
+    let width = entry.borderBoxSize[0].inlineSize;
+    let height = entry.borderBoxSize[0].blockSize;
+
+    if (cardIndex >= 0) {
+      overlay.children[cardIndex].style.width = `${width}px`;
+      overlay.children[cardIndex].style.height = `${height}px`;
+    }
+  });
+});
+
+const initOverlayCard = (cardEl) => {
+  const overlayCard = document.createElement("div");
+  overlayCard.classList.add("card");
+  createOverlayCta(overlayCard, cardEl.lastElementChild);
+  overlay.append(overlayCard);
+  observer.observe(cardEl);
+};
+
+cards.forEach(initOverlayCard);
+document.body.addEventListener("pointermove", applyOverlayMask);
